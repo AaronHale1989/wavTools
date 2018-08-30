c38a66fdbcfca08eee3c5b800f4873cac7cbf808
AaronHale1989 <halea02130@gmail.com>
2018-08-30 13:46:45 -0400

    s3

--- app/uploaders/audio_uploader.rb
+++ app/uploaders/audio_uploader.rb
@@ -2,6 +2,16 @@ class AudioUploader < CarrierWave::Uploader::Base
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
   # include CarrierWave::MiniMagick
+  if Rails.env.production?
+    CarrierWave.configure do |config|
+        config.fog_credentials = {
+          provider: => 'AWS',
+          aws_access_key_id: => ENV['S3_ACCESS_KEY'],
+          aws_secret_access_key: => ENV['S3_SECRET_KEY'],
+        }
+        config.fog_directory = ENV['S3_BUCKET']
+    end
+  end
 
   # Choose what kind of storage to use for this uploader:
   if Rails.env.production?
