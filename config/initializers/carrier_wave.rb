77d9e227e1ca4219449ff386ed63f41b35f6f17b
AaronHale1989 <halea02130@gmail.com>
2018-09-07 12:03:13 -0400

    more

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -1,8 +1,8 @@
 require 'fog/aws'
 if Rails.env.development?
   CarrierWave.configure do |config|
+    config.fog_provider = 'fog/aws'
       config.fog_credentials = {
-         :url  => ":s3_domain_url",
         :provider => 'AWS',
         :aws_access_key_id => ENV['S3_ACCESS_KEY'],
         :aws_secret_access_key => ENV['S3_SECRET_KEY'],
